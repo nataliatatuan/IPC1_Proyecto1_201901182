68566b76afb5dabf2fc8a63d5d6a62e4258a0920
nataliaixmatul <nataliaixmatul@gmail.com>
2024-03-05 21:55:57 -0600

    Proyecto 1 (primer commit)

---
+++ src/main/java/com/mycompany/proyecto1_ipc1/Proyecto1_IPC1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_ipc1;
+
+/**
+ *
+ * @author hp
+ */
+public class Proyecto1_IPC1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
